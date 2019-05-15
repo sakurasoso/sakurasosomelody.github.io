b0d6244cae5befe6403817772246746467c5915d
xns1997 <723648649@qq.com>
2019-05-15 10:49:47 -0700

    organized buttons

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var sound4  = document.getElementById("sound_4");
 var sound5  = document.getElementById("sound_5");
 var sound6  = document.getElementById("sound_6");
 
-ply_buttons[0].onclick = function(){ply_sd(sound1)};
+ply_buttons[0].onclick = function(){ply_1()};
 ply_buttons[1].onclick = function(){ply_sd(sound2)};
 ply_buttons[2].onclick = function(){ply_sd(sound3)};
 ply_buttons[3].onclick = function(){ply_sd(sound4)};
@@ -22,6 +22,12 @@ console.log(typeof(top_pos));
 buttons.style.top = top_pos;
 document.getElementsByTagName("button").oncontextmenu = function(){
     event.returnValue = false;
+}
+
+whole_html.onresize = function(){
+    var top_pos =  ((whole_html.offsetHeight - buttons.offsetHeight)/2) + "px";
+    console.log("Changed")
+    buttons.style.top = top_pos;
 }
 function ply(sound){
     sound.play();
@@ -37,4 +43,16 @@ function st(sound){
 function ply_sd(sound){
      sound.currentTime = 0;
      sound.cloneNode().play();
+}
+function ply_1(){
+    sound1.play()
+    sound1.currentTime = 0;
+    sound2.play()
+    sound2.currentTime = 0;
+    sound1.play()
+    sound1.currentTime = 0;
+    sound3.play()
+    sound3.currentTime = 0;
+    sound4.play()
+    sound4.currentTime = 0;
 }
