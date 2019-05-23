91e54fdaa7a742872c185ce652082927c3749b87
xns1997 <723648649@qq.com>
2019-05-23 13:33:57 -0700

    update

--- autoplay.js
+++ autoplay.js
@@ -10,9 +10,9 @@
 //8 - C
 var songscript_1 ="012345678";
 var songscript_2 ="333033303512300444404333032232050"+"3330333035123000444433355431000"//jingle bell
-var songscript_3 ="1155665044332210554433205544332011556654433221" //C C G G A A G 0 F F E E D D C 0 G G F F E E D 0 G G F F E E D 0 C C G G A A G 0 F F E E D D C
+var songscript_3 ="11556650443322105544332055443320115566504433221" //C C G G A A G 0 F F E E D D C 0 G G F F E E D 0 G G F F E E D 0 C C G G A A G 0 F F E E D D C
 var songlist = [songscript_1, songscript_2, songscript_3]
-var chosensong = songscript_2;
+var chosensong = songscript_3;
 var auto_button  = document.getElementById("start");
 auto_button.addEventListener("click",function(){auto_play(chosensong)})
 
