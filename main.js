27b03103958d8e5c25061c3cbdfded2ebf66cde2
Naisen Xu <723648649@qq.com>
2019-05-28 21:23:59 -0700

    shade

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 "use strict";
 var ply_buttons = document.getElementsByClassName("sound_button");
 
-var sound1  = document.getElementById("sound_1");
+/*var sound1  = document.getElementById("sound_1");
 var sound2  = document.getElementById("sound_2");
 var sound3  = document.getElementById("sound_3");
 var sound4  = document.getElementById("sound_4");
 var sound5  = document.getElementById("sound_5");
 var sound6  = document.getElementById("sound_6");
 var sound7  = document.getElementById("sound_7");
-var sound8  = document.getElementById("sound_8");
+var sound8  = document.getElementById("sound_8");*/
+
+var sound1 = new Audio('./kalimba/c3.mp3');
+var sound2 = new Audio('./kalimba/d3.wav');
+var sound3 = new Audio('./kalimba/e3.wav');
+var sound4 = new Audio('./kalimba/f3.wav');
+var sound5 = new Audio('./kalimba/g3.wav');
+var sound6 = new Audio('./kalimba/a3.wav');
+var sound7 = new Audio('./kalimba/b3.mp3');
+var sound8 = new Audio('./kalimba/c4.mp3');
+
 
 ply_buttons[0].onclick = function(){ply_sd(sound1);};
 ply_buttons[1].onclick = function(){ply_sd(sound2);};
@@ -22,6 +32,7 @@ ply_buttons[7].onclick = function(){ply_sd(sound8);};
 var whole_html = document.getElementsByTagName("html")[0];
 var buttons = document.getElementsByClassName("buttons")[0];
 var top_pos =  ((whole_html.offsetHeight - buttons.offsetHeight)/2) + "px";
+console.log((whole_html.offsetHeight - buttons.offsetHeight)/2);
 buttons.style.top = top_pos;
 document.getElementsByTagName("button").oncontextmenu = function(){
     event.returnValue = true;
@@ -47,6 +58,8 @@ function ply_sd(sound){
      sound.currentTime = 0;
      sound.cloneNode().play();
 }
+
+
 
 
 
